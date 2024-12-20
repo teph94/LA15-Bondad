e643d28ec3d5ec355b3a28d41becf41ab63d4725
denGit012 <deninja012@gmail.com>
2024-12-20 20:58:45 +0800

    first commit

---
+++ app1/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here. (request handler)
+def say_hello(request):
+    return HttpResponse('Hello Teph94')
+
+def say_hi(request):
+    return render(request, 'hi.html', {'name': 'Joseph Bondad'})
+
+def blog_list(request):
+    post = Post.objects.all()
+    context = {
+        'blog_list':post
+    }
+
+    return render(request, 'blog_list.html', context)
+
+def blog_detail(request, id):
+    each_post = Post.objects.get(id = id)
+    context = {
+        'blog_detail': each_post
+    }
+    return render(request, 'blog_detail.html', context)
